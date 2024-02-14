8377c39e337b43f8354280c890a49e54981b1621
aslesh6402 <adhikariaslesh@gmail.com>
2024-02-14 13:57:55 +0545

    track

--- main.py
+++ main.py
@@ -12,3 +12,19 @@ root.title("Phone Number Tracker")
 root.iconbitmap('Images/logo_PI6_icon.ico')
 root.geometry("365x584+200+100")
 root.resizable(False,False)
+
+def track():
+	entry_number=entry.get()
+	number=phonenumbers.parse(entry_number)
+	# country
+	locate=geocoder.description_for_number(number, 'en')
+	country.config(text=locate)
+	# operator 
+	operator=carrier.name_for_number(number, 'en')
+	sim.config(text=operator)
+	# Phone Timezone
+	time=timezone.time_zones_for_number(number)
+	zone.config(text=time)
+	# logitude and latitude 
+	geolocator=Nominatim(user_agent="geoapiExercises")
+	location=geolocator.geocode(locate)
